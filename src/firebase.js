f4d2275252be26836de8f63a6e296aad52841860
Faozan Segunmaru <segunfaozan@gmail.com>
2022-02-23 06:37:18 -0800

    COmpleted the header component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7EXHJXX4PKBgppyWUm6hYx-r2njuP3_A",
+    authDomain: "challenge-fb57d.firebaseapp.com",
+    projectId: "challenge-fb57d",
+    storageBucket: "challenge-fb57d.appspot.com",
+    messagingSenderId: "350756697550",
+    appId: "1:350756697550:web:6fa59cc3a572489faf279f",
+    measurementId: "G-X14N8PTTCQ"
+  }; 

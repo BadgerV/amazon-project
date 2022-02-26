ed6c9d9eb7b43d95cd542df61169ab91808b9fff
Faozan Segunmaru <segunfaozan@gmail.com>
2022-02-26 04:46:35 -0800

    continued with the project

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,18 +13,18 @@ function  Home() {
         </div>
 
         <div className="home__row">
-            <Product />
-            {/*Product*/}
+            <Product title = 'The lean Startup' price = {29.99} image = 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_SmartWatch_1X._SY304_CB639922137_.jpg' rating = {5} />
+            <Product title = 'Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 litre Glass Bowl' price = {239.99} image = 'https://m.media-amazon.com/images/I/61j17FjPhtL._AC_SY200_.jpg' rating = {4}/>
         </div>
 
         <div className="home__row">
-            {/*Product*/}
-            {/*Product*/}
-            {/*Product*/}
+            <Product title = 'HoMedics White Noise Sound Machine | Portable Sleep Therapy for Home, Office, Baby & Travel | 6 Relaxing & Soothing Nature Sounds.' price = {23.87} image = 'https://images-na.ssl-images-amazon.com/images/I/61XYX1tzoTL.__AC_SY300_SX300_QL70_FMwebp_.jpg' rating={5} />
+            <Product title = 'HBCyoU Homecoming Queen Nicole 18-inch Doll & Accessories, Wavy Hair, Deep Brown Skin Tone.' image = 'https://m.media-amazon.com/images/I/81eRi2w+9DL._AC_UL320_.jpg' rating={3} price={23.99}/>
+            <Product title= 'EpochAir Remote Control Car, 2.4GHz High Speed RC Cars, Offroad Hobby RC Racing Car with 2 Rechargeable Batteries' image = 'https://m.media-amazon.com/images/I/715Igl2a1eL._AC_UL320_.jpg' price = {24.15} rating = {1}/> 
         </div>
 
         <div className="home__row">
-            {/*Product*/}
+            <Product title = 'NPET K32 Wireless Gaming Keyboard, LED Backlit, Multimedia Keys, Silent Membrane Keyboard with Wrist Rest for Desktop, Computer, PC' image = 'https://m.media-amazon.com/images/I/71mKnxMOdmL._AC_UY218_.jpg' price ={50} rating = {4}/>
         </div>
     </div>
   )
--- src/product.jsx
+++ src/product.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 
 import './product.css'
 
-function product() {
+function product({title, image, price, rating}) {
   return (
     <div className='product'>
         <div className="product__info">
-            <p>The startup</p>
+            <p>{title}</p>
             <p className='product__price'>
                 <small>$</small>
-                <strong>29.99</strong>
+                <strong>{price}</strong>
             </p>
             <div className="product__rating">
+              {Array(rating).fill().map((_, i) => (
                 <p>⭐</p>
+              ))}
             </div>
         </div>
 
-        <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_SmartWatch_1X._SY304_CB639922137_.jpg" alt="" />
+        <img src={image} alt="" />
         <button>Add to Basket</button>
     </div>
   )

64f123de9f19b46313b2e2ca67d818c698a8864b
Faozan Segunmaru <segunfaozan@gmail.com>
2022-02-26 07:06:23 -0800

    continued

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 
 import './checkout.css'
 
+import Subtotal from './Subtotal'
+
 function CheckOut() {
     return <div className='checkout'>
         <div className="checkout__left">
             <img src="https://images-na.ssl-images-amazon.com/images/G/01/SmartHome/StorefrontRefresh/Plug_BannerCB422622693_.jpeg" alt="" className="checkout__ad" />
+            <div>
+                <h2 className="checkout__title">Your Shopping Basket</h2>
+
+                {/*BasketItem */}
+                {/*BasketItem */}
+                {/*BasketItem */}
+                {/*BasketItem */}
+            </div>
         </div>
 
         <div className="checkout__right">
-            
+            <Subtotal />
         </div>
     </div>
 }
